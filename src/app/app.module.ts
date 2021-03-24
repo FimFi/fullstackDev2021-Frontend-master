261145f3a724267e415c61b2b2c75c5be2d99247
FimFi <55874743+FimFi@users.noreply.github.com>
2021-03-24 10:23:52 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {SharedModule} from './shared/shared.module';
+
+
+
+
+const config: SocketIoConfig = { url: 'localhost:3100/', options: {} };
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SocketIoModule.forRoot(config),
+    BrowserAnimationsModule,
+    SharedModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
